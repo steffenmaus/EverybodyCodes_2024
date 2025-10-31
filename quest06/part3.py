bfc548501a656bf367c00be2fde9b6b99f33a642
Steffen Maus <steffenmaus@users.noreply.github.com>
2025-10-31 16:37:47 +0100

    Quest06.

---
+++ quest06/part3.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+with open('input3.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+root = "RR"
+children = defaultdict(list)
+powers = defaultdict(list)
+
+for l in lines:
+    k, v = l.split(":")
+    children[k] = [c for c in v.split(",") if c not in ("BUG", "ANT")]
+
+
+def f(node, path, depth):
+    for c in children[node]:
+        if c == "@":
+            powers[depth].append(path + "@")
+        else:
+            f(c, path + c[0], depth + 1)
+
+
+f(root, root[0], 0)
+
+ans = [p for p in powers.values() if len(p) == 1][0][0]
+print(ans)
