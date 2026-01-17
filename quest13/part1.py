e86e43aeafc4a904e1bc6098062cee6abdf95627
Steffen Maus <steffenmaus@users.noreply.github.com>
2026-01-17 11:28:09 +0100

    Quest13.

---
+++ quest13/part1.py
@@ -0,0 +1,54 @@
+import heapq
+
+with open('input1.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+
+def get_all_nei(p):
+    x, y = p
+    r = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+    r = [p for p in r if p in maze]
+    return r
+
+
+def get_dist(a, b):
+    a, b = sorted([a, b])
+    if b - a <= 5:
+        return b - a + 1
+    return a + 10 - b + 1
+
+
+def dijkstra(start):
+    out = {start: 0}
+    Q = []
+    for node in get_all_nei(start):
+        heapq.heappush(Q, (maze[node] + 1, node))
+    while Q:
+        dist, current = heapq.heappop(Q)
+        if current not in out:
+            out[current] = dist
+            for n in get_all_nei(current):
+                heapq.heappush(Q, (get_dist(maze[current], maze[n]) + dist, n))
+    return out
+
+
+X = len(lines[0])
+Y = len(lines)
+
+maze = {}
+start = None
+target = None
+for y in range(Y):
+    for x in range(X):
+        p = (x, y)
+        c = lines[y][x]
+        if c.isnumeric():
+            maze[p] = int(c)
+        elif c == "S":
+            start = p
+            maze[p] = 0
+        elif c == "E":
+            target = p
+            maze[p] = 0
+
+print(dijkstra(start)[target])
---
+++ quest13/part3.py
@@ -0,0 +1,56 @@
+import heapq
+
+with open('input3.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+
+def get_all_nei(p):
+    x, y = p
+    r = [(x, y + 1), (x, y - 1), (x + 1, y), (x - 1, y)]
+    r = [p for p in r if p in maze]
+    return r
+
+
+def get_dist(a, b):
+    a, b = sorted([a, b])
+    if b - a <= 5:
+        return b - a + 1
+    return a + 10 - b + 1
+
+
+def f(start, targets):
+    out = {start: 0}
+    Q = []
+    for node in get_all_nei(start):
+        heapq.heappush(Q, (maze[node] + 1, node))
+    while Q:
+        dist, current = heapq.heappop(Q)
+        if current not in out:
+            if current in targets:
+                return dist
+            out[current] = dist
+            for n in get_all_nei(current):
+                heapq.heappush(Q, (get_dist(maze[current], maze[n]) + dist, n))
+    return None
+
+
+X = len(lines[0])
+Y = len(lines)
+
+maze = {}
+starts = set()
+target = None
+for y in range(Y):
+    for x in range(X):
+        p = (x, y)
+        c = lines[y][x]
+        if c.isnumeric():
+            maze[p] = int(c)
+        elif c == "S":
+            starts.add(p)
+            maze[p] = 0
+        elif c == "E":
+            target = p
+            maze[p] = 0
+
+print(f(target, starts))
